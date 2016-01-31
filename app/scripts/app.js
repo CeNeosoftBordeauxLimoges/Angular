804e809fd5189275d7e5c6cbe4c40a73526e9225
CeNeosoftBordeauxLimoges <ce.neosoft.bordeaux.limoges@gmail.com>
2016-01-31 19:11:48 +0100

    Mise en place du server Node
    Modification de l'architecture du projet pour coller au "format" NodeJS.
    A ce stade, le serveur fait tourner le projet test Angular sans appel à
    la base mongoDB.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name adressBookApp
+ * @description
+ * # adressBookApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('adressBookApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adressBookApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the adressBookApp
+ */
+angular.module('adressBookApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
